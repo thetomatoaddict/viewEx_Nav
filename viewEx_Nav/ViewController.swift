12a1695e0129bcb277c2d1adfda93dd882ffd45a
sjlee <thetomatoaddict@gmail.com>
2022-11-16 18:17:30 +0900

    네비게이션 구현

--- viewEx_Nav/ViewController.swift
+++ viewEx_Nav/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func NavBtn(_ sender: UIButton) {
+        guard let redVC = storyboard?.instantiateViewController(withIdentifier: "RedVC") else { return }
+        navigationController?.pushViewController(redVC, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
